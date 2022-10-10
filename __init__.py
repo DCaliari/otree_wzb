8871f23e6781f87c451358c03d59bb56ff22d649
DCaliari <caliaridaniele@gmail.com>
2022-10-10 15:44:20 +0200

    First lecture

--- __init__.py
+++ __init__.py
@@ -36,15 +36,39 @@ class Choice(Page):
 
 
 class ChoiceWaitPage(WaitPage):  # wait pages html is automatically generated
-	pass
-
+	@staticmethod
+	def after_all_players_arrive(group: Group):
+		players = group.get_players()  # store the list of players in a local variables
+		contributions = [p.contribution for p in players]  # list of contributions per player
+		group.total = sum(contributions)
+		payout = group.total * C.MULTIPLIER
+		for p in players:
+			p.payoff = C.BUDGET - p.contribution + payout  # the payoff variable is pre-defined
+		
 
 class Feedback(Page):
-	pass
-
-
+	@staticmethod
+	def vars_for_template(player: Player):
+		others = player.get_others_in_group()
+		others_contributions = [o.contribution for o in others]
+		others_payoffs = [o.payoff for o in others]
+		return dict(
+			others=others,
+			others_contributions=others_contributions,
+			others_payoffs=others_payoffs
+		)
+		
+		
 class FinalFeedback(Page):
-	pass
+	@staticmethod
+	def is_displayed(player: Player):
+		return player.round_number == C.NUM_ROUNDS
 
 
 page_sequence = [Choice, ChoiceWaitPage, Feedback, FinalFeedback]
+
+
+# FUNCTIONS
+
+def creating_session(subsession: Subsession):
+	subsession.group_randomly()
